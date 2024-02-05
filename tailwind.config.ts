a049b57e6caaaf143335747c26c6890ba4e98556
devdahcoder <adigunolamide200@gmail.com>
2024-02-05 21:46:40 +0100

    build: setup husky, eslint and prettier for pre-commit and better code formatting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,33 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-    fontFamily: {
-      // "gordita": "gordita",
-      gordita: ["Gordita", "sans-serif"],
-    }
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			screens: {
+				'xs': '320px',
+				'sm': '480px',
+				'custom-sm': '559px',
+				'custom-md': '639px',
+				'md': '768px',
+				'lg': '1024px',
+				'xl': '1280px',
+				'2xl': '1920px',
+			},
+		},
+		fontFamily: {
+			gordita: ["Gordita", "sans-serif"],
+		}
+	},
+	plugins: [],
 };
 export default config;
