0dc024249b9afb8767c90ab0cf0aa87bb5910b40
devdahcoder <adigunolamide200@gmail.com>
2024-02-06 22:08:51 +0100

    refactor: refactored project page

--- src/app/(pages)/project/page.tsx
+++ src/app/(pages)/project/page.tsx
@@ -8,23 +8,41 @@ type Props = {};
 const Project = (props: Props) => {
 	return (
 		<SectionContainer>
-			<div className="flex-col justify-center items-start gap-8 flex w-full md:max-w-[80%] lg:max-w-[65%]">
-				<div className=" text-neutral-900 font-semibold text-3xl md:text-4xl lg:text-5xl leading-[3rem] md:leading-[4rem] lg:leading-[4rem]">
-					Develop user-friendly interfaces that make a positive
-					impact.
+			<div className="flex-col justify-center items-start gap-6 flex pb-20 md:pb-24">
+				<div
+					className={`text-3xl md:text-4xl lg:text-5xl font-semibold py-4 w-full md:w-[36rem] ls:w-[37rem] lg:w-[50rem] leading-[3rem] md:leading-[4rem] lg:leading-[4.5rem]`}>
+					<p>
+						Develop user-friendly interfaces{" "}
+						<br className="hidden md:block" /> that make a positive
+						impact.
+					</p>
 				</div>
-				<div className=" text-neutral-900 text-sm font-normal leading-[27px]">
-					Founded with a vision to revolutionize the world of software
-					development, we emerged as a beacon of innovation in the
-					tech industry. Our mission has been clear from the start to
-					deliver exceptional, customized software solutions that
-					propel businesses into the future.
+				<div className="leading-[1.7rem] w-full md:max-w-[80%] lg:w-[45rem]">
+					<p>
+						Founded with a vision to revolutionize the world of
+						software development, we emerged as a beacon of
+						innovation in the tech industry. Our mission has been
+						clear from the start to deliver exceptional, customized
+						software solutions that propel businesses into the
+						future.
+					</p>
 				</div>
 			</div>
 
-			<div className="flex flex-col gap-y-20 mt-32">
+			<div className="flex flex-col gap-y-28 mt-20">
 				{project.map((project, index) => (
-					<ProjectItem key={index} isEven={index % 2} {...project} />
+					<ProjectItem
+						key={index}
+						imageSrc={"/images/preview.png"}
+						projectDescription={`Vision Pay is an innovative agency banking app designed to make easy and secure banking transactions. Our development focused on user-centric design and reliable functionality.`}
+						projectName={project.title}
+						projectMotto={project.subtitle}
+						projectServices={project.skills}
+						containerClassName={""}
+						readCaseStudyLink={"/"}
+						viewProjectLink={"/"}
+						isEven={index % 2}
+					/>
 				))}
 			</div>
 		</SectionContainer>
--- src/components/templates/project.tsx
+++ src/components/templates/project.tsx
@@ -4,6 +4,7 @@ import ArrowIcon from "../../../public/icons/arrow-icon";
 import ImageBlock from "../atoms/image-block";
 import SectionHeader from "../atoms/section-header";
 import SectionContainer from "../molecules/section-container.";
+import ProjectItem from "../atoms/project-item";
 
 type Props = {};
 
@@ -16,55 +17,19 @@ const Project = (props: Props) => {
 							excellence 🔥"
 				subHeaderClassName="md:w-[40rem] lg:w-[70%]"
 			/>
-			<div className="flex flex-col md:flex-row items-center justify-between bg-gradient-to-r from-neutral-50 to-neutral-100 rounded-2xl px-3 md:px-12 py-9 md:py-7 gap-y-14 mt-20">
-				<div className="w-full md:w-[45%] text-center md:text-start flex flex-col items-center md:items-start ">
-					<div className="text-sm mb-5">
-						<p>Vision Pay</p>
-					</div>
-					<div className="text-2xl font-semibold bg-gradient-to-r from-slate-700 to-gray-800 bg-clip-text text-transparent">
-						<p>Empowering Agency Banking</p>
-					</div>
-					<div className="text-base my-5 bg-gradient-to-r from-slate-400 to-gray-400 bg-clip-text text-transparent leading-loose">
-						<p>
-							Vision Pay is an innovative agency banking app
-							designed to make easy and secure banking
-							transactions. Our development focused on
-							user-centric design and reliable functionality.
-						</p>
-					</div>
-					<div className="text-base font-medium bg-gradient-to-r from-gray-900 to-neutral-800 bg-clip-text text-transparent">
-						<p>UI/UX Design, Front & Back End Development</p>
-					</div>
 
-					<div>
-						<div className="mt-7 md:mt-5 flex flex-col min-[360px]:flex-row items-center ml-auto gap-x-3 gap-y-5">
-							<Link
-								href=""
-								id=""
-								className="group flex flex-row items-center gap-x-2 text-sm text-white no-underline capitalize px-4 py-2.5 font-medium rounded-full bg-gradient-to-r from-purple-500 to-purple-700 min-h-fit min-w-fit border border-[#9743FF] 
-						hover:text-[#141515] hover:bg-white transition duration-500 ease-in-out">
-								<span>View Project</span>{" "}
-								<span>
-									<ArrowIcon className="-rotate-45 w-4 h-4 stroke-white group-hover:stroke-[#141515] transition duration-500 ease-in-out" />
-								</span>
-							</Link>
-							<Link
-								href=""
-								id=""
-								className="text-sm text-primary-blue no-underline capitalize px-4 py-2.5 font-medium rounded-full bg-primary-white min-h-fit min-w-fit border border-[#FBF3EF] hover:text-white hover:bg-gradient-to-r from-zinc-700 to-gray-600 transition duration-500 ease-in-out">
-								Read Case Study
-							</Link>
-						</div>
-					</div>
-				</div>
-
-				<ImageBlock
-					subContainerClassName="block overflow-hidden relative !h-[25rem] !w-[19rem] min-[400px]:!w-[20rem] md:!w-[23rem] lg:!w-[25rem] !rounded-3xl"
-					className="absolute top-0 left-0 "
-					src="/images/preview.png"
-					alt="project-image"
-				/>
-			</div>
+			<ProjectItem
+				imageSrc={"/images/preview.png"}
+				projectDescription={`Vision Pay is an innovative agency banking app designed to make easy and secure banking transactions. Our development focused on user-centric design and reliable functionality.`}
+				projectName={"Vision Pay"}
+				projectMotto={"Empowering Agency Banking"}
+				projectServices={"UI/UX Design, Front & Back End Development"}
+				containerClassName={
+					"py-9 md:py-7 mt-20 md:flex-row bg-gradient-to-r from-neutral-50 to-neutral-100"
+				}
+				readCaseStudyLink={"/"}
+				viewProjectLink={"/"}
+			/>
 		</SectionContainer>
 	);
 };
