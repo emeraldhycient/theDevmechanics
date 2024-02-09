7f9d86d007fd0ce4003c2f4044b1c2d034ffcd3a
devdahcoder <adigunolamide200@gmail.com>
2024-02-09 08:59:11 +0100

    feat: added animation to header and hero section

--- src/components/molecules/header.tsx
+++ src/components/molecules/header.tsx
@@ -1,19 +1,40 @@
 "use client";
 
+import Links from "@/json/nav.json";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
-import Links from "@/json/nav.json";
-import { IoReorderTwoOutline } from "react-icons/io5";
+import React, { useEffect, useRef, useState } from "react";
 import { GoArrowRight } from "react-icons/go";
+import { IoReorderTwoOutline } from "react-icons/io5";
+import { elementObserver } from "../../../hooks";
+import gsap from "gsap";
 
 const Header = (): React.ReactNode => {
 	const [mobileNavOpen, setMobileNavOpen] = useState<boolean>(false);
+	const headerRefElement = useRef<HTMLDivElement>(null);
+
+	const animateHeaderSubContainer = () => {
+		gsap.fromTo(
+			".header-sub-container",
+			{ yPercent: -150, opacity: 0.3 },
+			{ yPercent: 0, duration: 1.5, ease: "sine.out", opacity: 1 },
+		);
+	};
+
+	useEffect(() => {
+		elementObserver(headerRefElement.current, (entry, observer) => {
+			if (entry.isIntersecting) {
+				animateHeaderSubContainer();
+			}
+			observer.unobserve(entry.target);
+		});
+	}, []);
 
 	return (
-		<header
+		<div
+			ref={headerRefElement}
 			className={` duration-200 py-4 sticky top-0 left-0 text-sm z-[99999999] bg-white`}>
-			<div className="w-11/12 md:w-10/12 mx-auto flex items-center justify-between">
+			<div className="header-sub-container w-11/12 md:w-10/12 mx-auto flex items-center justify-between">
 				<Link href={"/"}>
 					<Image
 						src={`/images/logo.svg`}
@@ -30,7 +51,7 @@ const Header = (): React.ReactNode => {
 							{Links.map((link, index) => (
 								<li key={index}>
 									<Link
-										className="p-0.5 flex flex-row items-center justify-center"
+										className="header-link p-0.5 flex flex-row items-center justify-center"
 										href={link.href}>
 										<p>{link.name}</p>
 									</Link>
@@ -94,7 +115,7 @@ const Header = (): React.ReactNode => {
 					</nav>
 				</div>
 			</div>
-		</header>
+		</div>
 	);
 };
 
