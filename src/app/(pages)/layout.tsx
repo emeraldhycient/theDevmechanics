fc52e47a5d2b7e02eb9761b074f61692f0380e86
devdahcoder <adigunolamide200@gmail.com>
2024-02-11 13:51:48 +0100

    build: added and setup lenis-scroll package

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,14 +1,18 @@
+"use client";
 import { Header } from "@/components/molecules";
 import { Footer } from "@/components/templates";
+import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<main>
-			<Header />
-			{children}
-			<Footer />
-		</main>
+		<ReactLenis root>
+			<main>
+				<Header />
+				{children}
+				<Footer />
+			</main>
+		</ReactLenis>
 	);
 };
 
